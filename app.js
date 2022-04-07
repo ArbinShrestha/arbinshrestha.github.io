45fc45e752c22faab5267510d2eeefe08ebca58f
orbeenshr <orbeenshr@gmail.com>
2022-04-07 17:50:01 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelectorAll('.main-content');
+console.log('all sections',allSections)
+
+PageTransitions = ()=>{
+    //Button click active
+    for(let i = 0; i < sectBtn.length; i++) {
+        sectBtn[i].addEventListener('click', function(){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
+        })
+    }
+
+    //Sections Activeclass
+    for(let sec of allSections){
+        sec.addEventListener('click', (e)=>{
+            const id = e.target.dataset.id;
+            if(id){
+                //remove selected from other btns
+                sectBtns.forEach((btn)=>{
+                    btn.classList.remove('active');
+                })
+                e.target.classList.add('active');
+
+                //hide other sections
+                sections.forEach((section)=>{
+                    section.classList.remove('active');
+                })
+                
+                const elements = document.getElementById(id);
+                elements.classList.add('active');
+            }
+        })
+    }
+  
+}
+
+PageTransitions()
+
