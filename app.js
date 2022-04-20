b79ee0bf7e3349aea61d37e369e897d3cd4406cb
orbeenshr <orbeenshr@gmail.com>
2022-04-20 14:55:05 +0545

    finished contact section

--- app.js
+++ app.js
@@ -35,6 +35,13 @@ PageTransitions = ()=>{
             }
         })
     }
+
+    //Toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', ()=>{
+        let element = document.body;
+        element.classList.toggle('light-mode');
+    })
   
 }
 
